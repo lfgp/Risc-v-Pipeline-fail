961e550abdb175cf01938ba3dc89fe493614aab0
lfgp <48449255+lfgp@users.noreply.github.com>
2025-02-17 21:00:21 -0300

    Update ALUController.sv
    reajustando controlador de ALU

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -11,18 +11,36 @@ module ALUController (
 );
 
   assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  // R\I-or
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100)) || // R\I-xor
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
+      ((ALUOp == 2'b01) && (Funct3 == 3'b001)) || // BNE
+      ((ALUOp == 2'b01) && (Funct3 == 3'B101)) || // BGE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b111));   // BGEU
 
-  assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
+    assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW\JALR-add
       ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
+      ((ALUOp == 2'b10) && (Funct3==3'b100)) || // R\I-xor
+      ((ALUOp == 2'b10) && (Funct7 == 7'b0100000) && (Funct3 == 3'b000)) ||  // R-sub
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
+       (ALUOp == 2'b11) ||   // JAL\LUI: return1
+      ((ALUOp == 2'b10) && (Funct3 == 3'b011)) || // SLTIU, SLTU: <u
+      ((ALUOp == 2'b01) && (Funct3 == 3'b110)) || // BLTU
+      ((ALUOp == 2'b01) && (Funct3 == 3'b111));   // BGEU
 
   assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
+      ((ALUOp == 2'b10) && (Funct7 == 7'b0100000) && (Funct3 == 3'b000)) ||  // R-sub
       ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
+      ((ALUOp == 2'b10) && (Funct3 == 3'b011)) || // SLTIU, SLTU: <u
+      ((ALUOp == 2'b01) && (Funct3 == 3'b110)) || // BLTU
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
+      ((ALUOp == 2'b01) && (Funct3 == 3'B101)) || // BGE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b111));   // BGEU
 
-  assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
+    assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ, BNE, BLT, BGE, BLTU, BGEU
+        (ALUOp == 2'b11) ||   // JAL\LUI: return1
       ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
+      ((ALUOp == 2'b10) && (Funct3 == 3'b011));   // SLTIU, SLTU: <u
 endmodule
