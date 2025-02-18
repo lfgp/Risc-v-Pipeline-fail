23f275e3df4688c5fe39e00f5ade3dc97f0275e7
lfgp <48449255+lfgp@users.noreply.github.com>
2025-02-17 21:23:50 -0300

    Update alu.sv
    reajustando alu

--- design/alu.sv
+++ design/alu.sv
@@ -17,28 +17,35 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
-	    4'b0001:        // OR
-    		    ALUResult = SrcA | SrcB;
+            4'b0001:        // OR
+                    ALUResult = SrcA | SrcB;
             4'b0010:        // ADD
-                    ALUResult = SrcA + SrcB;
-	    4'b0011: // Shift Logical Left
-    		    ALUResult = SrcA << SrcB;
-	    4'b0100: // Set Less Than
-    		    ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 1 : 0;
-	    4'b0101: // Set Less Than Unsigned
-    		    ALUResult = (SrcA < SrcB) ? 1 : 0;
-	    4'b0110: // SUB
-    		    ALUResult = $signed(SrcA) - $signed(SrcB);
-	    4'b0111: // LESS THAN
-    		    ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 1 : 0;
+                    ALUResult = $signed(SrcA) + $signed(SrcB);
+            4'b0011:        // XOR
+                    ALUResult = SrcA ^ SrcB;
+            4'b0100:        // Left Shift
+                    ALUResult = SrcA << SrcB[4:0];
+            4'b0101:        // Right Shift
+                    ALUResult = SrcA >> SrcB[4:0];
+            4'b0110:        // Subtract
+                    ALUResult = $signed(SrcA) - $signed(SrcB);
+            4'b0111:        // Right Shift Arithm
+                    ALUResult = $signed(SrcA) >>> SrcB[4:0];
+
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
-	    4'b1001: // XOR
-    		    ALUResult = SrcA ^ SrcB;
-	    4'b1010: // Shift Right Logical
-    		    ALUResult = SrcA >> SrcB;
-	    4'b1011: // Shift Right Arithmetic
-    		    ALUResult = $signed(SrcA) >>> SrcB;
+            4'b1001:        // Not Equal
+                    ALUResult = (SrcA != SrcB) ? 1 : 0;
+            4'b1100:        // Less Than
+                    ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 1 : 0;
+            4'b1101:        // Greater/Equal Than
+                    ALUResult = ($signed(SrcA) >= $signed(SrcB)) ? 1 : 0;
+            4'b1110:        // Unsigned Less Than
+                    ALUResult = (SrcA < SrcB) ? 1 : 0;
+            4'b1111:        // Unsigned Greater/Equal Than
+                    ALUResult = (SrcA >= SrcB) ? 1 : 0;
+            4'b1010:        // Always True, for jal
+                    ALUResult = 1;
             default:
                     ALUResult = 0;
             endcase
